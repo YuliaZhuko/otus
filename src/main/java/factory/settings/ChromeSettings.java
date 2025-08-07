69fe7cb641bc79417ff325c4c632828776cf3279
YuliaZhuko <spartanez@list.ru>
2025-08-07 14:21:21 +0300

    change chrome on chromium

--- src/main/java/factory/settings/ChromeSettings.java
+++ src/main/java/factory/settings/ChromeSettings.java
@@ -8,6 +8,7 @@ public class ChromeSettings implements IBrowserSettings {
   @Override
   public ChromeOptions setting() {
     ChromeOptions chromeOptions = new ChromeOptions();
+    chromeOptions.setBinary("/usr/bin/chromium-browser");
     chromeOptions.addArguments("--start-fullscreen");
     return chromeOptions;
   }
